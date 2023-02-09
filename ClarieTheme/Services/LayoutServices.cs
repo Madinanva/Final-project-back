1c4e5ec64cd1a65de3d2949cb830c75a7281de92
Madina <madinarn@code.edu.az>
2023-02-10 03:02:04 +0400

    first commit

---
+++ ClarieTheme/Services/LayoutServices.cs
@@ -0,0 +1,23 @@
+﻿using ClarieTheme.DAL;
+using ClarieTheme.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClarieTheme.Services
+{
+    //public class LayoutServices : ILayoutServices
+    //{
+    //    private readonly AppDbContext _context;
+    //    public LayoutServices(AppDbContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public async Task<Dictionary<string, string>> GetSettingsAsync()
+    //    {
+    //        return await _context.Settings.ToDictionaryAsync(s => s.Key, s => s.Value);
+    //    }
+    //}
+}
